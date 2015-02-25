71727f37249712b0b30504e9bac5e6500168ab2c
David Shaevel <info@davidshaevel.com>
2015-02-24 23:52:23 -0600

    Starting to use express

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var _alApac = require('./app/alApac');
+var _alApac = require('./app/alApac'),
+	_bodyParser = require('body-parser'),
+	_express = require('express'),
+	_util = require('util');
 
-_alApac.run();
+var app = _express();
+app.use(_bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+	res.send({
+		name: 'actionList-apac'
+	});
+});
+
+app.post('/doStuff', function(req, res) {
+	_alApac.run();
+
+	res.send({
+		description: 'called alApac.run()'
+	});
+});
+
+app.listen(3000);
+
 
